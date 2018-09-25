82943722aadec6bc3419e06f4ccc577c56fb3f4a
Chao Yu <yuchao0@huawei.com>
2018-09-25 15:35:58 +0800

    f2fs: fix to recover inode's project id during POR
    [ Upstream commit f4474aa6e5e901ee4af21f39f1b9115aaaaec503 ]
    
    Testcase to reproduce this bug:
    1. mkfs.f2fs -O extra_attr -O project_quota /dev/sdd
    2. mount -t f2fs /dev/sdd /mnt/f2fs
    3. touch /mnt/f2fs/file
    4. sync
    5. chattr -p 1 /mnt/f2fs/file
    6. xfs_io -f /mnt/f2fs/file -c "fsync"
    7. godown /mnt/f2fs
    8. umount /mnt/f2fs
    9. mount -t f2fs /dev/sdd /mnt/f2fs
    10. lsattr -p /mnt/f2fs/file
    
        0 -----------------N- /mnt/f2fs/file
    
    But actually, we expect the correct result is:
    
        1 -----------------N- /mnt/f2fs/file
    
    The reason is we didn't recover inode.i_projid field during mount,
    fix it.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -238,34 +238,20 @@ static int recover_inode(struct inode *inode, struct page *page)
 {
 	struct f2fs_inode *raw = F2FS_INODE(page);
 	char *name;
-	int err;
 
 	inode->i_mode = le16_to_cpu(raw->i_mode);
-
-	err = recover_quota_data(inode, page);
-	if (err)
-		return err;
-
 	i_uid_write(inode, le32_to_cpu(raw->i_uid));
 	i_gid_write(inode, le32_to_cpu(raw->i_gid));
 
 	if (raw->i_inline & F2FS_EXTRA_ATTR) {
-		if (f2fs_sb_has_project_quota(F2FS_I_SB(inode)) &&
+		if (f2fs_sb_has_project_quota(F2FS_I_SB(inode)->sb) &&
 			F2FS_FITS_IN_INODE(raw, le16_to_cpu(raw->i_extra_isize),
 								i_projid)) {
 			projid_t i_projid;
-			kprojid_t kprojid;
 
 			i_projid = (projid_t)le32_to_cpu(raw->i_projid);
-			kprojid = make_kprojid(&init_user_ns, i_projid);
-
-			if (!projid_eq(kprojid, F2FS_I(inode)->i_projid)) {
-				err = f2fs_transfer_project_quota(inode,
-								kprojid);
-				if (err)
-					return err;
-				F2FS_I(inode)->i_projid = kprojid;
-			}
+			F2FS_I(inode)->i_projid =
+				make_kprojid(&init_user_ns, i_projid);
 		}
 	}
 
@@ -279,23 +265,14 @@ static int recover_inode(struct inode *inode, struct page *page)
 
 	F2FS_I(inode)->i_advise = raw->i_advise;
 	F2FS_I(inode)->i_flags = le32_to_cpu(raw->i_flags);
-	f2fs_set_inode_flags(inode);
-	F2FS_I(inode)->i_gc_failures[GC_FAILURE_PIN] =
-				le16_to_cpu(raw->i_gc_failures);
-
-	recover_inline_flags(inode, raw);
-
-	f2fs_mark_inode_dirty_sync(inode, true);
 
 	if (file_enc_name(inode))
 		name = "<encrypted>";
 	else
 		name = F2FS_INODE(page)->i_name;
 
-	f2fs_msg(inode->i_sb, KERN_NOTICE,
-		"recover_inode: ino = %x, name = %s, inline = %x",
-			ino_of_node(page), name, raw->i_inline);
-	return 0;
+	f2fs_msg(inode->i_sb, KERN_NOTICE, "recover_inode: ino = %x, name = %s",
+			ino_of_node(page), name);
 }
 
 static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head,
