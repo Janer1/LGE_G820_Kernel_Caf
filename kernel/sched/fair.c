befdf7191d572dfb32d02b062a67d653517e028c
Rick Yiu <rickyiu@google.com>
2020-06-30 14:43:33 +0800

    sched/fair: schedule lower priority tasks from little cores
    With scheduler placement hint, there still could be several boosted
    tasks contending for big cores. On chipset with fewer big cores, it
    might cause problems like jank. To improve it, schedule tasks of prio
    >= DEFAULT_PRIO from little cores if they could fit, even for tasks
    that prefer high capacity cpus, since such prio means they are less
    important.
    
    Bug: 158936596
    Test: tasks scheduled as expected
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ic0cc06461818944e3e97ec0493c0d9c9f1a5e217
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7426,11 +7426,13 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	unsigned int margin;
 
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
-		margin = schedtune_task_boost(p) > 0 ?
-			sched_capacity_margin_down_boosted[task_cpu(p)] :
-			sched_capacity_margin_down[task_cpu(p)];
+		margin = schedtune_task_boost(p) > 0 &&
+			   p->prio <= DEFAULT_PRIO ?
+			sched_capacity_margin_down_boosted[cpu] :
+			sched_capacity_margin_down[cpu];
 	else
-		margin = schedtune_task_boost(p) > 0 ?
+		margin = schedtune_task_boost(p) > 0 &&
+			   p->prio <= DEFAULT_PRIO ?
 			sched_capacity_margin_up_boosted[task_cpu(p)] :
 			sched_capacity_margin_up[task_cpu(p)];
 
@@ -7928,8 +7930,14 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		 */
 		if ((prefer_idle && best_idle_cpu != -1) ||
 		    (boosted && (best_idle_cpu != -1 || target_cpu != -1))) {
-			if (boosted) {
-				if (!next_group_higher_cap)
+			if (boosted && p->prio <= DEFAULT_PRIO) {
+				/*
+				 * For boosted task, stop searching when an idle
+				 * cpu is found in mid cluster.
+				 */
+				if ((mid_cap_orig_cpu != -1 &&
+					best_idle_cpu >= mid_cap_orig_cpu) ||
+					!next_group_higher_cap)
 					break;
 			} else {
 				if (next_group_higher_cap)
@@ -8259,7 +8267,9 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int placement_boost = task_boost_policy(p);
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
-	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
+	int boosted = (schedtune_task_boost(p) > 0 && p->prio <= DEFAULT_PRIO)
+							|| per_task_boost(p) > 0;
+	bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 	fbt_env.fastpath = 0;
 	fbt_env.need_idle = 0;
