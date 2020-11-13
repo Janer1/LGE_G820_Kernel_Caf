b87a32b1e672763b6b8261c706fa0c4120278451
Anh Tran <anh.tran.3889@gmail.com>
2020-11-13 16:19:13 +0700

    Enable F2FS

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ mkdir -p out
 make O=./out ARCH=arm64 vendor/alpha_lao_com-perf_defconfig
 make O=./out ARCH=arm64 REAL_CC=clang -j16
 cp -f ./out/arch/arm64/boot/Image.gz-dtb ./release/Dragonfly/Image.gz-dtb
-cd release/Dragonfly
-zip -r Dragonfly.zip *
-mv Dragonfly.zip ../Dragonfly.zip
