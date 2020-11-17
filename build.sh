9029634d0bdb4add5ffb527128252609aa91f071
Anh Tran <anh.tran.3889@gmail.com>
2020-11-18 00:20:13 +0700

    Fix Internal Problem issue by using stock defconfig for /proc/config.gz

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo rm -rf ./out
 mkdir -p out
-make O=./out ARCH=arm64 vendor/alpha_lao_com-perf_defconfig
+make O=./out ARCH=arm64 dragonfly_defconfig
 make O=./out ARCH=arm64 REAL_CC=clang -j16
 cp -f ./out/arch/arm64/boot/Image.gz-dtb ./release/Dragonfly/Image.gz-dtb
